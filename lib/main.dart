49f48dc77b48cae42d23813d3640d8f008781158
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-12-28 20:26:45 -0300

    Project init and some methods

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+
+  runApp(MaterialApp(
+
+    home: Home()
+  ));
+}
+
+class Home extends StatefulWidget {
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  List _todoList = [];
+
+  FutureOr<File> _getFile() async {
+
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  FutureOr<File> _saveData() async {
+
+    String data = json.encode(_todoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  FutureOr<String> _readData() async {
+
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+
+    } catch(e) {
+      return null;
+
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container();
+  }
+}
