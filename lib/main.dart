c70a8f8ef0b7cbef780b8aa1115a1f0be3f405da
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-12-29 18:10:23 -0300

    Criação do input de entrada e botão de adição

--- lib/main.dart
+++ lib/main.dart
@@ -5,51 +5,68 @@ import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
 void main() {
-
-  runApp(MaterialApp(
-
-    home: Home()
-  ));
+  runApp(MaterialApp(home: Home()));
 }
 
 class Home extends StatefulWidget {
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-
   List _todoList = [];
 
   FutureOr<File> _getFile() async {
-
     final directory = await getApplicationDocumentsDirectory();
     return File("${directory.path}/data.json");
   }
 
   FutureOr<File> _saveData() async {
-
     String data = json.encode(_todoList);
     final file = await _getFile();
     return file.writeAsString(data);
   }
 
   FutureOr<String> _readData() async {
-
     try {
       final file = await _getFile();
       return file.readAsString();
-
-    } catch(e) {
+    } catch (e) {
       return null;
-
     }
   }
 
   @override
   Widget build(BuildContext context) {
-
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Lista de Tarefas"),
+          centerTitle: true,
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          labelText: "Nova tarefa",
+                          labelStyle: TextStyle(color: Colors.blueAccent)),
+                    ),
+                  ),
+                  RaisedButton(
+                    color: Colors.blueAccent,
+                    child: Text("ADD"),
+                    textColor: Colors.white,
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
